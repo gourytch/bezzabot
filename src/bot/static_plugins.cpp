4424c467ec3d285348678cd620a4decd6ace5f3b
Lev V. Babchenko <gour@mail.ru>
2012-01-11 01:12:14 +0600

    добавлен незаписанный контент

--- src/bot/static_plugins.cpp
+++ src/bot/static_plugins.cpp
@@ -1,1 +1,13 @@
+#include <QApplication>
+#include <QtPlugin>
+
+Q_IMPORT_PLUGIN(qjpeg)
+Q_IMPORT_PLUGIN(qgif)
+Q_IMPORT_PLUGIN(qico)
+Q_IMPORT_PLUGIN(qsvg)
+Q_IMPORT_PLUGIN(qtiff)
+Q_IMPORT_PLUGIN(qdecorationdefault)
+Q_IMPORT_PLUGIN(qdecorationwindows)
+
+
 
