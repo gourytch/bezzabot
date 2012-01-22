29a8d8286841ac1db890536da5a662e3222f99bf
Lev V. Babchenko <gour@mail.ru>
2012-01-22 13:55:47 +0600

    REBUILD-win32.cmd was changed

--- REBUILD-win32.cmd
+++ REBUILD-win32.cmd
@@ -1,4 +1,7 @@
-@ECHO OFF
-DEL /F /S /Q LIB OBJ BIN\*.EXE
-QMAKE -recursive
-MAKE all
+DEL /F /S /Q LIB OBJ BIN\*.EXE 
+
+REM DEL /F /S /Q src\tools\build_id.h
+REM git checkout src/tools/build_id.h
+
+qmake -recursive
+make all
