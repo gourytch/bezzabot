cd6d2510435f16d16a06a20066a71dff278528fb
Lev V. Babchenko <gour@mail.ru>
2012-02-17 02:44:37 +0600

    once more.

--- REBUILD-win32.cmd
+++ REBUILD-win32.cmd
@@ -3,7 +3,7 @@ DEL /F /S /Q LIB OBJ BIN\*.EXE
 REM DEL /F /S /Q src\tools\build_id.h
 REM git checkout src/tools/build_id.h
 
-PATH C:\QtSDK\Desktop\Qt\4.7.3\mingw\bin;C:\QtSDK\mingw\bin;%PATH%
+PATH C:\QtSDK\Desktop\Qt\4.7.3\mingw\bin;C:\QtSDK\mingw\bin;C:\MinGW\bin;C:\MinGW\mingw32\bin;C:\MinGW\msys\1.0\bin;C:\MinGW\msys\1.0\sbin;%PATH%
 
 qmake -recursive
 make -j2 all
