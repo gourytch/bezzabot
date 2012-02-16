9ca206ba2ceeab6aefb293a4f8b160d6141f3174
Lev V. Babchenko <gour@mail.ru>
2012-02-17 02:36:37 +0600

    added path to REBUILD-win32.cmd

--- REBUILD-win32.cmd
+++ REBUILD-win32.cmd
@@ -3,5 +3,7 @@ DEL /F /S /Q LIB OBJ BIN\*.EXE
 REM DEL /F /S /Q src\tools\build_id.h
 REM git checkout src/tools/build_id.h
 
+PATH C:\QtSDK\Desktop\Qt\4.7.3\mingw\bin;C:\QtSDK\mingw\bin;%PATH%
+
 qmake -recursive
 make -j2 all
