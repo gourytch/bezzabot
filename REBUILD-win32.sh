b2843be87ab89f12bfa328ebdefdd37d577b9758
Lev V. Babchenko <gour@mail.ru>
2012-02-17 02:12:54 +0600

    add missing #2

--- REBUILD-win32.sh
+++ REBUILD-win32.sh
@@ -4,5 +4,6 @@ make clean;
 rm -f bin/bezzabot.exe bin/surgery.exe bin/test_disp.exe bin/test_parsers.exe
 rm -rf obj/ lib/
 i686-pc-mingw32-qmake -recursive &&
-make clean all
+make clean &&
+make -j2 all
 
--- REBUILD.sh
+++ REBUILD.sh
@@ -4,5 +4,6 @@ make clean;
 rm -f bin/bezzabot bin/surgery bin/test_disp bin/test_parsers
 rm -rf obj/ lib/
 qmake -recursive &&
-make clean all
+make clean && 
+make -j2 all 
 
