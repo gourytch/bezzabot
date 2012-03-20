6c12f5502ed2d0534a1207a14301b5e5ef6a4f92
Lev V. Babchenko <gour@mail.ru>
2012-03-20 17:47:13 +0600

    BUILD-bb-lx32.sh deploys to beee

--- BUILD-bb-lx32.sh
+++ BUILD-bb-lx32.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 sudo linux32 chroot /srv/chroots/squeeze32 /bin/bash --login -c "cd ~/bezzabot && git pull && ./REBUILD.sh"
+if [ -e /srv/chroots/squeeze32/root/bezzabot/bin/bezzabot ]
+then
+  cp /srv/chroots/squeeze32/root/bezzabot/bin/bezzabot /mnt/auto/sshfs/beee-gour/home/gour/bezzabot/bezzabot-i686
+  echo "OK"
+else
+  echo "FAIL"
+fi
 
