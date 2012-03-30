5a2b1d32bb36c1c06efa1fb1bcf04dfca684248b
Lev V. Babchenko <gour@mail.ru>
2012-03-31 02:42:37 +0600

    parallel building was been removed from win32-cmd

--- REBUILD-win32.cmd
+++ REBUILD-win32.cmd
@@ -6,4 +6,4 @@ REM git checkout src/tools/build_id.h
 PATH C:\QtSDK\Desktop\Qt\4.7.3\mingw\bin;C:\QtSDK\mingw\bin;C:\MinGW\bin;C:\MinGW\mingw32\bin;C:\MinGW\msys\1.0\bin;C:\MinGW\msys\1.0\sbin;%PATH%
 
 qmake -recursive
-make -j2 all
+make all
